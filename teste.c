014d74aad79b8dbd0ad64a4c9c46c33cf67f4e3b
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-10 01:06:46 -0300

    teste calculo

---
+++ teste.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{ 
+	printf("Qual tamanho da sua matriz quadrada?\n");
+	scanf("%i", &n);
+	int mtrx[n][n];
+	int i,j;
+	printf("Digite os valores de sua matriz separando os valores por linha,tecle enter ao terminar a linha:\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("\n");		
+		for(j = 0; j < n; j++)
+		{
+			scanf("%i", mtrx[i][j]);
+			printf("%i", mtrx[i][j]);
+		}
+	}
+	return 0;
+}	
+
