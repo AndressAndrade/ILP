288ad8714e7cc4e1182cc52960c235b0a534ba92
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-10 12:27:18 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
-
 int main()
-{ 
-	int i,j,n;
-	printf("Qual tamanho da sua matriz quadrada?\n");
-	scanf("%i", &n);
-	int mtrx[n][n];
-	printf("Digite os valores de sua matriz separando os valores por linha,tecle enter ao terminar a linha:\n");
-	for (i = 0; i < n; i++)
-	{
-		printf("\n");		
-		for(j = 0; j < n; j++)
-		{
-			scanf(" %i ", &mtrx[i][j]);
-			printf(" %i", mtrx[i][j]);
-		}
-	}
-	return 0;
+{
+    int i,j,n;
+    printf("Qual tamanho da sua matriz quadrada?\n");
+    scanf("%i", &n);
+    int mtrx[n][n];
+    printf("Digite os valores de sua matriz separando os valores por linha,tecle enter ao terminar a linha:\n");
+    for (i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%i ", &mtrx[i][j]);
+        }
+    }
+    printf("\n");
+    for (i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            printf("%i ", mtrx[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+
 }
