201e0797e3ad1acb9392c35ad4a8a78b4bd9221e
Andressa Andrade <andressa.brasil-tokyo@hotmail.com>
2014-10-10 01:12:58 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -2,20 +2,19 @@
 
 int main()
 { 
+	int i,j,n;
 	printf("Qual tamanho da sua matriz quadrada?\n");
 	scanf("%i", &n);
 	int mtrx[n][n];
-	int i,j;
 	printf("Digite os valores de sua matriz separando os valores por linha,tecle enter ao terminar a linha:\n");
 	for (i = 0; i < n; i++)
 	{
 		printf("\n");		
 		for(j = 0; j < n; j++)
 		{
-			scanf("%i", mtrx[i][j]);
+			scanf("%i", &mtrx[i][j]);
 			printf("%i", mtrx[i][j]);
 		}
 	}
 	return 0;
-}	
-
+}
